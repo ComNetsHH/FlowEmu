80d55c93642399334a2d82b0efe1a45255bd7f6e
Daniel Stolpmann <daniel.stolpmann@tuhh.de>
2021-02-02 15:23:44 +0100

    Improve style and refactor

--- src/modules/loss/UncorrelatedLossModule.cpp
+++ src/modules/loss/UncorrelatedLossModule.cpp
@@ -2,18 +2,18 @@
 
 using namespace std;
 
-UncorrelatedLossModule::UncorrelatedLossModule(Mqtt &mqtt, double p, uint32_t loss_seed) : generator_loss(), mqtt(mqtt) {
+UncorrelatedLossModule::UncorrelatedLossModule(Mqtt &mqtt, double p, uint32_t seed) : mqtt(mqtt) {
 	setLossProbability(p);
-	setLossSeed(loss_seed);
+	setSeed(seed);
 
 	mqtt.subscribe("set/uncorrelated_loss/loss_probability", [&](const string &topic, const string &message) {
 		double loss = stod(message);
 		setLossProbability(loss);
 	});
 
-	mqtt.subscribe("set/uncorrelated_loss/loss_seed", [&](const string &topic, const string &message) {
-		uint32_t loss_seed = stoul(message);
-		setLossSeed(loss_seed);
+	mqtt.subscribe("set/uncorrelated_loss/seed", [&](const string &topic, const string &message) {
+		uint32_t seed = stoul(message);
+		setSeed(seed);
 	});
 }
 
@@ -25,12 +25,12 @@ void UncorrelatedLossModule::setLossProbability(double p) {
 	mqtt.publish("get/uncorrelated_loss/loss_probability", to_string(p), true);
 }
 
-void UncorrelatedLossModule::setLossSeed(uint32_t loss_seed) {
-	this->loss_seed = loss_seed;
+void UncorrelatedLossModule::setSeed(uint32_t seed) {
+	this->seed = seed;
 
-	generator_loss.seed(loss_seed);
+	generator_loss.seed(seed);
 
-	mqtt.publish("get/uncorrelated_loss/loss_seed", to_string(loss_seed), true);
+	mqtt.publish("get/uncorrelated_loss/seed", to_string(seed), true);
 }
 
 void UncorrelatedLossModule::receiveFromLeftModule(shared_ptr<Packet> packet) {
--- src/modules/loss/UncorrelatedLossModule.hpp
+++ src/modules/loss/UncorrelatedLossModule.hpp
@@ -1,9 +1,9 @@
 #ifndef UNCORRELATED_LOSS_MODULE_HPP
 #define UNCORRELATED_LOSS_MODULE_HPP
 
-#include <vector>
 #include <cstdint>
 #include <random>
+#include <atomic>
 #include <memory>
 
 #include <boost/asio.hpp>
@@ -14,17 +14,18 @@
 
 class UncorrelatedLossModule : public ModuleHasLeft<std::shared_ptr<Packet>>, public ModuleHasRight<std::shared_ptr<Packet>> {
 	public:
-		UncorrelatedLossModule(Mqtt &mqtt, double p, uint32_t loss_seed = 1);
+		UncorrelatedLossModule(Mqtt &mqtt, double p, uint32_t seed = 1);
 
 		void setLossProbability(double p);
-		void setLossSeed(uint32_t loss_seed);
+		void setSeed(uint32_t seed);
 
 		void receiveFromLeftModule(std::shared_ptr<Packet> packet) override;
 		void receiveFromRightModule(std::shared_ptr<Packet> packet) override;
 	private:
 		Mqtt &mqtt;
+
 		std::atomic<double> p;
-		std::atomic<uint32_t> loss_seed;
+		std::atomic<uint32_t> seed;
 
 		std::default_random_engine generator_loss;
 		std::unique_ptr<std::bernoulli_distribution> distribution;
