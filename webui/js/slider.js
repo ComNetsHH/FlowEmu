fe1f4a1c96067aeda3df993997ef992b3ffe2d32
Daniel Stolpmann <daniel.stolpmann@tuhh.de>
2021-02-11 10:33:08 +0100

    Generalize sliders

--- webui/js/slider.js
+++ webui/js/slider.js
@@ -1,52 +1,31 @@
 $(function(){
-	// Delay
-	subscribe("get/fixed_delay/delay", function(topic, payload) {
-		var value = parseInt(payload);
+	$(".parameter").each(function() {
+		var box = $(this);
+		var label = $(this).children(".parameter_value").first();
+		var slider = $(this).children(".parameter_slider").first();
 
-		$("#delay_value").text(value);
-		$("#delay_slider").data("current_value", value);
+		subscribe("get/" + box.attr("parameter"), function(topic, payload) {
+			var value = parseInt(payload);
 
-		if($("#delay_slider").data("clicked") != true) {
-			$("#delay_slider").val(value);
-		}
-	});
+			label.text(value);
+			slider.data("current_value", value);
 
-	$("#delay_slider").on("input", function() {
-		publish("set/fixed_delay/delay", $(this).val());
-		$(this).data("clicked", true);
+			if(slider.data("clicked") != true) {
+				slider.val(value);
+			}
+		});
 
-		var timeout = $(this).data("timeout");
-		clearTimeout(timeout);
-		timeout = setTimeout( function(element) {
-			element.data("clicked", false);
-			element.val(element.data("current_value"));
-		}, 1000, $(this));
-		$(this).data("timeout", timeout);
-	});
+		slider.on("input", function() {
+			publish("set/" + box.attr("parameter"), $(this).val());
+			$(this).data("clicked", true);
 
-	// Loss
-	subscribe("get/uncorrelated_loss/loss_probability", function(topic, payload) {
-		var value_p = parseFloat(payload);
-		var value = parseInt(value_p * 10000);
-
-		$("#loss_value").text(value_p * 100);
-		$("#loss_slider").data("current_value", value);
-
-		if($("#loss_slider").data("clicked") != true) {
-			$("#loss_slider").val(value);
-		}
-	});
-
-	$("#loss_slider").on("input", function() {
-		publish("set/uncorrelated_loss/loss_probability", ($(this).val() / 10000).toString());
-		$(this).data("clicked", true);
-
-		var timeout = $(this).data("timeout");
-		clearTimeout(timeout);
-		timeout = setTimeout( function(element) {
-			element.data("clicked", false);
-			element.val(element.data("current_value"));
-		}, 1000, $(this));
-		$(this).data("timeout", timeout);
+			var timeout = $(this).data("timeout");
+			clearTimeout(timeout);
+			timeout = setTimeout( function(element) {
+				element.data("clicked", false);
+				element.val(element.data("current_value"));
+			}, 1000, $(this));
+			$(this).data("timeout", timeout);
+		});
 	});
 });
