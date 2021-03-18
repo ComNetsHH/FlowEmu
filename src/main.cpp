83df2234a5f52f5d0098cc72dae3e6d683dd9840
Daniel Stolpmann <daniel.stolpmann@tuhh.de>
2021-03-18 18:38:55 +0100

    Remove all modules from build except for raw sockets

--- src/main.cpp
+++ src/main.cpp
@@ -8,18 +8,18 @@
 #include <boost/asio.hpp>
 
 #include "modules/ModuleManager.hpp"
-#include "modules/delay/FixedDelayModule.hpp"
-#include "modules/loss/GilbertElliotLossModule.hpp"
-#include "modules/loss/UncorrelatedLossModule.hpp"
-#include "modules/meter/DelayMeter.hpp"
-#include "modules/meter/ThroughputMeter.hpp"
-#include "modules/null/NullModule.hpp"
-#ifdef MACHINE_LEARNING
-#include "modules/queue/DQLQueueModule.hpp"
-#endif
-#include "modules/rate/BitrateRateModule.hpp"
-#include "modules/rate/FixedIntervalRateModule.hpp"
-#include "modules/rate/TraceRateModule.hpp"
+// #include "modules/delay/FixedDelayModule.hpp"
+// #include "modules/loss/GilbertElliotLossModule.hpp"
+// #include "modules/loss/UncorrelatedLossModule.hpp"
+// #include "modules/meter/DelayMeter.hpp"
+// #include "modules/meter/ThroughputMeter.hpp"
+// #include "modules/null/NullModule.hpp"
+// #ifdef MACHINE_LEARNING
+// #include "modules/queue/DQLQueueModule.hpp"
+// #endif
+// #include "modules/rate/BitrateRateModule.hpp"
+// #include "modules/rate/FixedIntervalRateModule.hpp"
+// #include "modules/rate/TraceRateModule.hpp"
 #include "modules/socket/RawSocket.hpp"
 #include "utils/Mqtt.hpp"
 
@@ -55,35 +55,35 @@ int main(int argc, const char *argv[]) {
 
 	// Modules
 	//BitrateRateModule bitrate_rate_module(io_service, mqtt, 1000000, 100);
-	FixedIntervalRateModule fixed_interval_rate_module(io_service, mqtt, chrono::milliseconds(1), 100);
+	//FixedIntervalRateModule fixed_interval_rate_module(io_service, mqtt, chrono::milliseconds(1), 100);
 	//TraceRateModule trace_rate_module(io_service, mqtt, "/config/traces/trace.down", "/config/traces/trace.up", 100);
-	#ifdef MACHINE_LEARNING
-	DQLQueueModule dql_queue_module(io_service, mqtt, chrono::milliseconds(1));
-	#endif
+	//#ifdef MACHINE_LEARNING
+	//DQLQueueModule dql_queue_module(io_service, mqtt, chrono::milliseconds(1));
+	//#endif
 	//NullModule null_module;
-	UncorrelatedLossModule uncorrelated_loss_module(mqtt, 0);
+	//UncorrelatedLossModule uncorrelated_loss_module(mqtt, 0);
 	//GilbertElliotLossModule gilbert_elliot_loss_module(io_service, mqtt, 1.0/10000, 1.0/100, 0, 0.1);
-	FixedDelayModule fixed_delay_module(io_service, mqtt, 0);
-	DelayMeter delay_meter_module(io_service, mqtt);
-	ThroughputMeter throughput_meter_module_left(io_service, mqtt, "throughput_meter_module_left");
-	ThroughputMeter throughput_meter_module_right(io_service, mqtt, "throughput_meter_module_right");
+	//FixedDelayModule fixed_delay_module(io_service, mqtt, 0);
+	//DelayMeter delay_meter_module(io_service, mqtt);
+	//ThroughputMeter throughput_meter_module_left(io_service, mqtt, "throughput_meter_module_left");
+	//ThroughputMeter throughput_meter_module_right(io_service, mqtt, "throughput_meter_module_right");
 
 	// Connect modules
-	module_manager.push_back(&socket_source);
-	module_manager.push_back(&throughput_meter_module_left);
+	//module_manager.push_back(&socket_source);
+	//module_manager.push_back(&throughput_meter_module_left);
 	//module_manager.push_back(&bitrate_rate_module);
-	module_manager.push_back(&fixed_interval_rate_module);
+	//module_manager.push_back(&fixed_interval_rate_module);
 	//module_manager.push_back(&trace_rate_module);
-	#ifdef MACHINE_LEARNING
-	module_manager.push_back(&dql_queue_module);
-	#endif
+	//#ifdef MACHINE_LEARNING
+	//module_manager.push_back(&dql_queue_module);
+	//#endif
 	//module_manager.push_back(&null_module);
-	module_manager.push_back(&uncorrelated_loss_module);
+	//module_manager.push_back(&uncorrelated_loss_module);
 	//module_manager.push_back(&gilbert_elliot_loss_module);
-	module_manager.push_back(&fixed_delay_module);
-	module_manager.push_back(&delay_meter_module);
-	module_manager.push_back(&throughput_meter_module_right);
-	module_manager.push_back(&socket_sink);
+	//module_manager.push_back(&fixed_delay_module);
+	//module_manager.push_back(&delay_meter_module);
+	//module_manager.push_back(&throughput_meter_module_right);
+	//module_manager.push_back(&socket_sink);
 
 	// MQTT subscriptions
 	mqtt.subscribeJson("set/module/+", [&](const string &topic, const Json::Value &json_root) {
