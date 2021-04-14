9eae419a7db83812a478ff2b92848d346c50d28f
Daniel Stolpmann <daniel.stolpmann@tuhh.de>
2021-04-14 18:34:05 +0200

    Add notify signal from responding to requesting port

--- src/modules/Port.hpp
+++ src/modules/Port.hpp
@@ -106,6 +106,10 @@ template<typename T> class RequestingPort : virtual public Port {
 			this->connected_port = nullptr;
 		}
 
+		void setNotifyHandler(std::function<void()> handler) {
+			this->notify_handler = handler;
+		}
+
 		T request() {
 			if(this->connected_port == nullptr) {
 				return T();
@@ -114,8 +118,17 @@ template<typename T> class RequestingPort : virtual public Port {
 			return connected_port->request();
 		}
 
+		void notify() {
+			try {
+				notify_handler();
+			} catch(const std::bad_function_call &e) {
+			}
+		}
+
 	protected:
 		RespondingPort<T>* connected_port = nullptr;
+
+		std::function<void()> notify_handler;
 };
 
 template<typename T> class RespondingPort : virtual public Port {
@@ -148,6 +161,14 @@ template<typename T> class RespondingPort : virtual public Port {
 			}
 
 			return T();
+		}
+
+		void notify() {
+			if(this->connected_port == nullptr) {
+				return;
+			}
+
+			return connected_port->notify();
 		}
 
 	protected:
